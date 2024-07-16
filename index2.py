ac7b7a1c016f179ab3513497f467a20951f033f9
yllinjoooZ <yllzeneli920@gmail.com>
2024-07-16 01:47:01 -0700

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,7 @@
+country = ["Kosovo", "Germany","Switzerland","Albania","Usa"]
+print("Lista me shtete:", country)
+sorted_country = sorted(country)
+print("Shtetet sipas renditjes alfabetike:", sorted_country)
+country.remove("Switzerland")
+print("Lista me shtetet pas fshirjes se 'Switzerland':", country)
+print("Printo vetem 'Albania':","Albania")
